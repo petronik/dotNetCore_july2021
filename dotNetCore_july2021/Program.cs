71accf9ef2baf3d098a6dd2e44a7ba9572bbdc65
Petronik <petronik@ukr.net>
2021-07-20 13:44:04 -0500

    added ArrayList

--- dotNetCore_july2021/Program.cs
+++ dotNetCore_july2021/Program.cs
@@ -1,41 +1,46 @@
 ﻿using System;
+using System.Collections;
+using System.Numerics;
 using System.Text.RegularExpressions;
 
 namespace dotNetCore_july2021
 {
     class Program
     {
-        static void PrintIsValid(string input, string pattern)
+        static void PrintValues(IEnumerable myList)
         {
-            Regex re = new Regex($@"{pattern}");
-
-            Console.WriteLine($"{input} matches {pattern}: {re.IsMatch(input)}");
+            foreach (Object obj in myList)
+            {
+                Console.Write(" {0}", obj);
+            }
+        }
+        static void ToWords(string numb)
+        {
+            ArrayList toWord = new ArrayList();
+            for (int i = 0; i < numb.Length; i++)
+            {
+                char ch = numb[i];
+                switch (ch)
+                {
+                    case (char)1: toWord.Add("one") ; break;
+                    case (char)2: toWord.Add("two"); break;
+                    case (char)3: toWord.Add("three"); break;
+                    case (char)4: toWord.Add("four"); break;
+                    case (char)5: toWord.Add("five"); break;
+                    case (char)6: toWord.Add("six"); break;
+                    case (char)7: toWord.Add("seven"); break;
+                    case (char)8: toWord.Add("eight"); break;
+                    case (char)9: toWord.Add("nine"); break;
+                }
+            }
+            Console.Write($"{numb} is " );
+            PrintValues(toWord);
         }
 
         static void Main(string[] args)
         {
-            ConsoleKeyInfo cki;
-            while (true)
-            {
-                string customRegEx = "";
-                string customVal = "";
-                Console.WriteLine("Enter a regular expression (or press ENTER to use the default):");
-                customRegEx = Console.ReadLine();
-                if(customRegEx == "")
-                {
-                    customRegEx = "^[a-z]+$";   
-                }
-                Console.WriteLine("Enter some input: ");
-                customVal = Console.ReadLine();
-                PrintIsValid(customVal, customRegEx);
+            ToWords("123");
 
-                Console.WriteLine("\nPress ESC to end or any key to try again.");
-                cki = Console.ReadKey();
-                if (cki.Key == ConsoleKey.Escape)
-                {
-                    break;
-                }
-            }
         }
     }
 }
