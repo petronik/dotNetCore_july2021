34a0bbffc71d475deab56f7b55ab3f89ca770aee
Petronik <petronik@ukr.net>
2021-07-21 09:00:19 -0500

    added List instead ArrayList

--- dotNetCore_july2021/Program.cs
+++ dotNetCore_july2021/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.Numerics;
 using System.Text.RegularExpressions;
 
@@ -16,25 +17,36 @@ namespace dotNetCore_july2021
         }
         static void ToWords(string numb)
         {
-            ArrayList toWord = new ArrayList();
+            List<string> toWord = new List<string>();
+            string word = string.Empty;
             for (int i = 0; i < numb.Length; i++)
             {
-                char ch = numb[i];
-                switch (ch)
+                if(numb[i] == 1)
                 {
-                    case (char)1: toWord.Add("one") ; break;
-                    case (char)2: toWord.Add("two"); break;
-                    case (char)3: toWord.Add("three"); break;
-                    case (char)4: toWord.Add("four"); break;
-                    case (char)5: toWord.Add("five"); break;
-                    case (char)6: toWord.Add("six"); break;
-                    case (char)7: toWord.Add("seven"); break;
-                    case (char)8: toWord.Add("eight"); break;
-                    case (char)9: toWord.Add("nine"); break;
+                    toWord.Add("one");
                 }
+
+                
+                //char ch = numb[i];
+                //if (Char(ch).Equals("o"))
+                //{
+                //    Console.WriteLine(ch);
+                //}
+                //switch (numb[i])
+                //{
+                //    case (char)1: toWord.Add("one"); break;
+                //    case (char)2: toWord.Add("two"); break;
+                //    case (char)3: toWord.Add("three"); break;
+                //    case (char)4: toWord.Add("four"); break;
+                //    case (char)5: toWord.Add("five"); break;
+                //    case (char)6: toWord.Add("six"); break;
+                //    case (char)7: toWord.Add("seven"); break;
+                //    case (char)8: toWord.Add("eight"); break;
+                //    case (char)9: toWord.Add("nine"); break;
+                //}
             }
             Console.Write($"{numb} is " );
-            PrintValues(toWord);
+            toWord.ForEach(num => Console.Write(num + " "));
         }
 
         static void Main(string[] args)
