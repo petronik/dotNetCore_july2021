ba3aca548774f184de4b8bc9edfc83173b7784f0
Petronik <petronik@ukr.net>
2021-07-23 10:57:44 -0400

    added oueNumber

--- dotNetCore_july2021/Program.cs
+++ dotNetCore_july2021/Program.cs
@@ -8,54 +8,72 @@ namespace dotNetCore_july2021
 {
     class Program
     {
+        private static readonly String[] unitsMap = new[] {
+            "", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+            "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen",
+            "seventeen", "eighteen", "nineteen"
+        };
 
-        static void bunchOfThreeToWords(string part) // part = 185
+        private static readonly String[] tensArr = new string[] {
+            "", "ten", "twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninty" };
+
+        private static readonly string[] suffixesArr = new string[] { "", "thousand", "million", "billion",
+            "trillion", "quadrillion", "quintillion", "sextillion", "septillion", "octillion",
+            "nonillion", "decillion", "undecillion", "duodecillion", "tredecillion",
+            "Quattuordecillion", "Quindecillion", "Sexdecillion", "Septdecillion",
+            "Octodecillion", "Novemdecillion", "Vigintillion" };
+        static string ThreeToWords(string part) // part = 185
         {
-            //string words = "";
-            //if (part.length < 3)
-            //{
-            //    words = part.insert(1, "f");
-            //}
+            part = part.PadLeft(3, '0');
+            string res = string.Empty;
+            if(part[0] != '0')
+            {
+                res += unitsMap[Convert.ToInt32(part[0].ToString())]
+                    + " hundred";
+            }
+            if (part[1] == '1')
+            {
+                int ind = Convert.ToInt32(part[1].ToString() + part[2].ToString());
+                string teen = unitsMap[ind];
 
-            //Console.WriteLine(words);
+                res += string.IsNullOrEmpty(res) ? teen : " and " + teen;
+            }
+            else
+            {
+                int ind = Convert.ToInt32(part[1].ToString());
+                int ind2 = Convert.ToInt32(part[2].ToString());
+                string tens = tensArr[ind];
+                string single = unitsMap[ind2];
+
+                res += string.IsNullOrEmpty(res) ?
+                    string.IsNullOrEmpty(tens) ? single : tens + " " + single
+                    : " and " + tens + " " + single;
+            }
+            return res;
         }
             static void Main(string[] args)
         {
             string input = "18500000000000000000000000000";
-            Console.WriteLine(input);
+            
             BigInteger bi = BigInteger.Parse(input);
 
             string num = bi.ToString("N0");
 
-            Console.WriteLine(" This is num:" + num);
+            string[] partsByThree = num.Split(" ");// 
 
-            string[] partsByThree = num.Split(" ");// 
+            string ourNumber = string.Empty;
 
-
-            Console.WriteLine("This is first index " + partsByThree[0]);
-            foreach (var part in partsByThree)
+            for (var i = 0; i < partsByThree.Length; ++i)
             {
-                Console.Write("PART IS " + part);
-                //bunchOfThreeToWords(part);
+                string str = ThreeToWords(partsByThree[i]);
+
+                str = string.IsNullOrEmpty(str) ? ""
+                    : str + " " + suffixesArr[partsByThree.Length - i - 1];
+                ourNumber += str + " ";
             }
-            char ch = ' ';
-            Console.WriteLine( "Character" + ch);
+            Console.WriteLine($"ourNumber is {ourNumber}");
 
-
-            //string[] numbersArr = new string[] { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
-            //string[] tensArr = new string[] { "twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninty" };
-            //string[] suffixesArr = new string[] { "thousand", "million", "billion", "trillion", "quadrillion", "quintillion", "sextillion", "septillion", "octillion", "nonillion", "decillion", "undecillion", "duodecillion", "tredecillion", "Quattuordecillion", "Quindecillion", "Sexdecillion", "Septdecillion", "Octodecillion", "Novemdecillion", "Vigintillion" };
-
-            //switch (counter)
-            //{
-            //    case 1: words += NumWords(decPart) + " tenths"; break;
-            //    case 2: words += NumWords(decPart) + " hundredths"; break;
-            //    case 3: words += NumWords(decPart) + " thousandths"; break;
-            //    case 4: words += NumWords(decPart) + " ten-thousandths"; break;
-            //    case 5: words += NumWords(decPart) + " hundred-thousandths"; break;
-            //    case 6: words += NumWords(decPart) + " millionths"; break;
-            //    case 7: words += NumWords(decPart) + " ten-millionths"; break;
-            //}
+            
 
             Console.WriteLine("End of program.");
         }
