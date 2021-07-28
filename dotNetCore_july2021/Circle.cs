9ec575742ce23b42b23849b33aab2d22e0702764
Petronik <petronik@ukr.net>
2021-07-28 16:54:55 -0700

    addad Shape class

---
+++ dotNetCore_july2021/Circle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNetCore_july2021
+{
+    class Circle
+    {
+        private double Area;
+        public Circle() { }
+        public Circle(double Radius)
+        {
+
+        }
+
+    }
+}
---
+++ dotNetCore_july2021/Shape.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNetCore_july2021
+{
+    public class Shape
+    {
+        private string Area;
+        public Shape() { }
+        public Shape(double Area)
+        {
+
+        }
+    }
+}
