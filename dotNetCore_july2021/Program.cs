054fe2cfebbd50db17904894dd0188670a012091
Petronik <petronik@ukr.net>
2021-07-28 11:08:21 -0700

    added methods: convert to and from xml

--- dotNetCore_july2021/Program.cs
+++ dotNetCore_july2021/Program.cs
@@ -1,29 +1,108 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Text;
+using System.Text.Json;
+using System.Xml.Serialization;
 
-namespace StringBuilder2
+namespace FileSystem4
 {
+    public class Item
+    {
+        public string id { get; set; }
+        public string label { get; set; }
+    }
+
+    public class Menu
+    {
+        public string header { get; set; }
+        [XmlElement]
+        public List<Item> items { get; set; }
+    }
+
+    public class Top
+    {
+        public Menu menu { get; set; }
+
+        public void Print()
+        {
+            Console.WriteLine(menu.header);
+            foreach (var i in menu.items)
+            {
+                if (i != null)
+                {
+                    if (i.id != null)
+                        Console.WriteLine(i.id);
+                    if (i.label != null)
+                        Console.WriteLine(" " + i.label);
+                }
+                else
+                    Console.WriteLine("null");
+
+            }
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            string passage = @"I do not like them
-                                In a house.
-                                I do not like them
-                                With a mouse.
-                                I do not like them
-                                Here or there.
-                                I do not like them
-                                Anywhere.
-                                I do not like green eggs and ham.
-                                I do not like them, Sam - I - am.";
+            // string json = Console.ReadLine();
+            string file = @"data.json";
 
-            StringBuilder sbp = new StringBuilder(passage);
+            string jsonString = File.ReadAllText(file);
 
-            sbp.Replace("not ", null);
+            // Console.WriteLine(json);
 
-            Console.WriteLine(sbp.ToString());
+            Top obj = JsonSerializer.Deserialize<Top>(jsonString);
+            //Top obj = JsonConvert.DeserializeObject<Top>(jsonString);
+
+
+            obj.Print();
+
+            /*
+            var xs = new XmlSerializer(typeof(Top));
+            string xmlFile = @"data.xml";
+
+            using (FileStream fs = File.Create(xmlFile))
+            {
+                xs.Serialize(fs, obj);
+            }
+            */
+
+            ToXmlFile("sample1.xml", obj);
+
+            Console.WriteLine("==================================");
+
+            var resObj = FromXmlFile<Top>("sample1.xml");
+            resObj.Print();
+
+
 
         }
+
+        public static T FromXmlFile<T>(string file)
+        {
+            XmlSerializer xmls = new XmlSerializer(typeof(T));
+            var xmlContent = File.ReadAllText(file);
+
+            using (StringReader sr = new StringReader(xmlContent))
+            {
+                return (T)xmls.Deserialize(sr);
+            }
+        }
+
+        public static void ToXmlFile<T>(string file, T obj)
+        {
+            using (StringWriter sw =
+                new StringWriter(new StringBuilder()))
+            {
+                XmlSerializer xmls = new XmlSerializer(typeof(T));
+                xmls.Serialize(sw, obj);
+                File.WriteAllText(file, sw.ToString());
+            }
+        }
+
+
+
     }
 }
