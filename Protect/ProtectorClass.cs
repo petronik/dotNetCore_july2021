277e68619e857569530dc59ece2d83e3d57fc896
Petronik <petronik@ukr.net>
2021-07-30 11:30:09 -0700

    added ProtectorClass

---
+++ Protect/ProtectorClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Security.Principal;
+using System.Text;
+using System.Xml.Linq;
+using static System.Convert;
+
+namespace Protector
+{
+     public class ProtectorClass
+    {
+        
+    }
+}
--- dotNetCore_july2021/Program.cs
+++ dotNetCore_july2021/Program.cs
@@ -1,15 +1,59 @@
 ﻿using static System.Console;
 using Protector;
+using System.IO;
+using System.Text;
+using System.Xml.Serialization;
+using System.Collections.Generic;
 
 namespace dotNetCore_july2021
 {
-    class Program
+    public class Program
     {
-        
+        [XmlInclude(typeof(Customer))]
+        public class Customer
+        {
+            public string Name { get; set; }
+            public string CardNumber { get; set; }
+            public string Password { get; set; }
+        }
 
         static void Main(string[] args)
         {
+            var listOfCustomers = new List<Customer>
+            {
+                new Customer {Name = "John Doe", CardNumber = "1233-4567-7890-1234", Password = "Pa$$w0rD"},
+                new Customer {Name = "Peter Pan", CardNumber = "0987-7654-4321-1092", Password = "Adm1n321"}
+            };
+
+            string xmlFile = "UnsecuredData.xml";
+            ToXmlFile(xmlFile, listOfCustomers);
+
+            List<Customer> file1 = FromXmlFile<List<Customer>>("UnsecuredData.xml");
+            foreach(Customer item in file1)
+            {
+                WriteLine($"{item.Name} ");
+            }
+        }
+
+        public static T FromXmlFile<T>(string file)
+        {
+            XmlSerializer xmls = new XmlSerializer(typeof(T));
+            var xmlContent = File.ReadAllText(file);
+
+            using(StringReader sr = new StringReader(xmlContent))
+            {
+                return (T)xmls.Deserialize(sr);
+            }
             
+        }
+        public static void ToXmlFile<T>(string file, T obj)
+        {
+            using (StringWriter sw = new StringWriter(new StringBuilder() ))
+            {
+                XmlSerializer xmls = new XmlSerializer(typeof(T));
+                xmls.Serialize(sw, obj);
+                File.WriteAllText(file, sw.ToString());
+            }
         }
     }
 }
