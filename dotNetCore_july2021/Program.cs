687cc832467c8698262f0ef89c421023ed7db41e
Petronik <petronik@ukr.net>
2021-08-02 11:37:27 -0700

    original data source file added

--- dotNetCore_july2021/Program.cs
+++ dotNetCore_july2021/Program.cs
@@ -10,48 +10,38 @@ namespace dotNetCore_july2021
     public class Program
     {
         [XmlInclude(typeof(Customer))]
+        [XmlInclude(typeof(Top))]
         public class Customer
         {
             public string Name { get; set; }
             public string CardNumber { get; set; }
             public string Password { get; set; }
         }
+        public class Customers
+        {
+            [XmlElement]
+            public List<Customer> customer { get; set; }
+        }
+        public class Top
+        {
+            public Customers customers { get; set; }
+            public void Print()
+            {
+                WriteLine(customers);
+                foreach(var c in customers.customer)
+                {
+                    WriteLine($"{c.Name} \n {c.CardNumber} \n {c.Password}");
+                }
+            }
+        }
 
         static void Main(string[] args)
         {
-            var listOfCustomers = new List<Customer>
-            {
-                new Customer {Name = "John Doe", CardNumber = "1233-4567-7890-1234", Password = "Pa$$w0rD"},
-                new Customer {Name = "Peter Pan", CardNumber = "0987-7654-4321-1092", Password = "Adm1n321"}
-            };
 
-            string xmlFile = "UnsecuredData.xml";
-            ToXmlFile(xmlFile, listOfCustomers);
+            string file = "OriginalData.xml";
+            Top obj = FromXmlFile<Top>() ;
 
-            List<Customer> file1 = FromXmlFile<List<Customer>>("UnsecuredData.xml");
-
-            // string sk = ProtectedClass.GenerateSecretKey();
-            string sk = @"\SBO;FK`y4O_fdi8\cj=]uyKnoUC0C\<";
-            for (int i = 0; i < file1.Count; i++) //encripting card number and hashing password
-            {
-                var encrCardNum = ProtectorClass.EncryptString(sk, file1[i].CardNumber);
-                file1[i].CardNumber = encrCardNum;
-                string hashed = ProtectorClass.SaltAndHash(file1[i].Password);
-                file1[i].Password = hashed;
-            }
-
-            string ProtectedFile = "protectedFile.xml";
-            ToXmlFile(ProtectedFile, file1);// creates file with protected data
-
-            WriteLine("Unsecured list of data");
-            foreach (Customer item in listOfCustomers)
-            {
-                WriteLine($"Custormer {item.Name} has card nimber {item.CardNumber} and password {item.Password}");
-            }
-            WriteLine("Protected list of data");
-            WriteLine(FromXmlFile <Customer>(ProtectedFile));
-
-
+            WriteLine(File.ReadAllText(file));
         }
 
         public static T FromXmlFile<T>(string file)
