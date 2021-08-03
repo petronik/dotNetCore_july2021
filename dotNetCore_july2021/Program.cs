f5ef2e6c13adf5f56847cda3efd6490905b902de
Petronik <petronik@ukr.net>
2021-08-03 12:47:10 -0700

    added decriptive method

--- dotNetCore_july2021/Program.cs
+++ dotNetCore_july2021/Program.cs
@@ -9,39 +9,55 @@ namespace dotNetCore_july2021
 {
     public class Program
     {
-        [XmlInclude(typeof(Customer))]
-        [XmlInclude(typeof(Top))]
+        //[XmlInclude(typeof(Customer))]
+        
         public class Customer
         {
+            [XmlElement("name")]
             public string Name { get; set; }
+            [XmlElement("creditcard")]
             public string CardNumber { get; set; }
+            [XmlElement("password")]
             public string Password { get; set; }
         }
+        [XmlRoot("customers")]
         public class Customers
         {
-            [XmlElement]
+            [XmlElement("customer")]
             public List<Customer> customer { get; set; }
+            
         }
-        public class Top
-        {
-            public Customers customers { get; set; }
-            public void Print()
-            {
-                WriteLine(customers);
-                foreach(var c in customers.customer)
-                {
-                    WriteLine($"{c.Name} \n {c.CardNumber} \n {c.Password}");
-                }
-            }
-        }
+        
 
         static void Main(string[] args)
         {
-
+            //var listofCustomers = new List<Customer> { };
             string file = "OriginalData.xml";
-            Top obj = FromXmlFile<Top>(file) ;
+            Customers obj = FromXmlFile<Customers>(file) ;
+            WriteLine("Unsecured list of data:");
+            foreach (var c in obj.customer )
+            {
+                WriteLine($"Custormer {c.Name} has card {c.CardNumber } and password {c.Password}");
+            }
 
-            WriteLine(File.ReadAllText(file));
+
+            string sk = @"\SBO;FK`y4O_fdi8\cj=]uyKnoUC0C\<";
+            WriteLine($"\nProtected Data:");
+            foreach (var c in obj.customer)
+            {
+                var encrCardNum = ProtectorClass.EncryptString(sk, c.CardNumber);
+                c.CardNumber = encrCardNum;
+                string hashed = ProtectorClass.SaltAndHash(c.Password);
+                c.Password = hashed;
+                WriteLine($"Customer {c.Name} has card {c.CardNumber} with password {c.Password}");
+            }
+            WriteLine("\nDecripted list of data");
+            foreach (var c in obj.customer)
+            {
+                string dString = ProtectorClass.DecryptString(sk, c.CardNumber);
+                WriteLine($"Custormer {c.Name} has card {dString} and password {c.Password}");
+            }
+
         }
 
         public static T FromXmlFile<T>(string file)
